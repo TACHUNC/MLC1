2629c3be9ba63a768ac37f8a23ec693979def621
TACHUNC <dannychendaqun@gmail.com>
2024-11-19 11:18:48 +0000

    push algo

---
+++ algo.py
@@ -0,0 +1,146 @@
+# import libraries
+import numpy as np
+import time
+import numpy.random as rnd
+from scipy.spatial.distance import cdist
+from scipy.optimize import minimize
+from scipy.stats import norm
+from scipy.stats import norm, qmc
+
+class GP_model:   
+    def __init__(self, X, Y, kernel, multi_hyper, var_out=True):
+        # GP variable definitions
+        self.X, self.Y, self.kernel = X, Y, kernel
+        self.n_point, self.nx_dim   = X.shape[0], X.shape[1]
+        self.ny_dim                 = Y.shape[1]
+        self.multi_hyper            = multi_hyper
+        self.var_out                = var_out
+        
+        # normalize data
+        self.X_mean, self.X_std     = np.mean(X, axis=0), np.std(X, axis=0)
+        self.Y_mean, self.Y_std     = np.mean(Y, axis=0), np.std(Y, axis=0)
+        self.X_norm, self.Y_norm    = (X-self.X_mean)/self.X_std, (Y-self.Y_mean)/self.Y_std
+        
+        # determine hyperparameters
+        self.hypopt, self.invKopt   = self.determine_hyperparameters()        
+    
+    def Cov_mat(self, kernel, X_norm, W, sf2):
+    
+        if kernel == 'RBF':
+            dist       = cdist(X_norm, X_norm, 'seuclidean', V=W)**2 
+            cov_matrix = sf2*np.exp(-0.5*dist)
+            return cov_matrix
+        else:
+            print('ERROR no kernel with name ', kernel)
+
+    def calc_cov_sample(self,xnorm,Xnorm,ell,sf2):   
+        # internal parameters
+        nx_dim = self.nx_dim
+
+        dist = cdist(Xnorm, xnorm.reshape(1,nx_dim), 'seuclidean', V=ell)**2
+        cov_matrix = sf2 * np.exp(-.5*dist)
+
+        return cov_matrix                
+    
+    
+    def negative_loglikelihood(self, hyper, X, Y):
+        # internal parameters
+        n_point, nx_dim = self.n_point, self.nx_dim
+        kernel          = self.kernel
+        
+        W               = np.exp(2*hyper[:nx_dim])   # W <=> 1/lambda
+        sf2             = np.exp(2*hyper[nx_dim])    # variance of the signal 
+        sn2             = np.exp(2*hyper[nx_dim+1])  # variance of noise
+
+        K       = self.Cov_mat(kernel, X, W, sf2)  # (nxn) covariance matrix (noise free)
+        K       = K + (sn2 + 1e-8)*np.eye(n_point) # (nxn) covariance matrix
+        K       = (K + K.T)*0.5                    # ensure K is simetric
+        L       = np.linalg.cholesky(K)            # do a cholesky decomposition
+        logdetK = 2 * np.sum(np.log(np.diag(L)))   # calculate the log of the determinant of K the 2* is due to the fact that L^2 = K
+        invLY   = np.linalg.solve(L,Y)             # obtain L^{-1}*Y
+        alpha   = np.linalg.solve(L.T,invLY)       # obtain (L.T L)^{-1}*Y = K^{-1}*Y
+        NLL     = np.dot(Y.T,alpha) + logdetK      # construct the NLL
+
+        return NLL
+    
+    def determine_hyperparameters(self):  
+        # internal parameters
+        X_norm, Y_norm  = self.X_norm, self.Y_norm
+        nx_dim, n_point = self.nx_dim, self.n_point
+        kernel, ny_dim  = self.kernel, self.ny_dim
+        Cov_mat         = self.Cov_mat
+        
+        lb               = np.array([-4.]*(nx_dim+1) + [-8.])  # lb on parameters (this is inside the exponential)
+        ub               = np.array([4.]*(nx_dim+1) + [ -2.])   # lb on parameters (this is inside the exponential)
+        bounds           = np.hstack((lb.reshape(nx_dim+2,1),
+                                      ub.reshape(nx_dim+2,1)))
+        
+        multi_start      = self.multi_hyper                   # multistart on hyperparameter optimization
+        
+        sampler = qmc.Sobol(d=nx_dim + 2,scramble=False)
+        multi_startvec = sampler.random(multi_start)
+        
+        # multi_startvec   = sobol_seq.i4_sobol_generate(nx_dim + 2,multi_start)
+
+        options  = {'disp':False,'maxiter':10000}          # solver options
+        hypopt   = np.zeros((nx_dim+2, ny_dim))            # hyperparams w's + sf2+ sn2 (one for each GP i.e. output var)
+        localsol = [0.]*multi_start                        # values for multistart
+        localval = np.zeros((multi_start))                 # variables for multistart
+
+        invKopt = []
+        # --- loop over outputs (GPs) --- #
+        for i in range(ny_dim):    
+            # --- multistart loop --- # 
+            for j in range(multi_start):
+                hyp_init    = lb + (ub-lb)*multi_startvec[j,:]
+                # --- hyper-parameter optimization --- #
+                res = minimize(self.negative_loglikelihood,hyp_init,args=(X_norm,Y_norm[:,i])\
+                               ,method='SLSQP',options=options,bounds=bounds,tol=1e-12)
+                localsol[j] = res.x
+                localval[j] = res.fun
+
+            # --- choosing best solution --- #
+            minindex    = np.argmin(localval)
+            hypopt[:,i] = localsol[minindex]
+            ellopt      = np.exp(2.*hypopt[:nx_dim,i])
+            sf2opt      = np.exp(2.*hypopt[nx_dim,i])
+            sn2opt      = np.exp(2.*hypopt[nx_dim+1,i]) + 1e-8
+
+            # --- constructing optimal K --- #
+            Kopt        = Cov_mat(kernel, X_norm, ellopt, sf2opt) + sn2opt*np.eye(n_point)
+            # --- inverting K --- #
+            invKopt     += [np.linalg.solve(Kopt,np.eye(n_point))]
+
+        return hypopt, invKopt
+    
+    def GP_inference_np(self, x):
+        nx_dim                   = self.nx_dim
+        kernel, ny_dim           = self.kernel, self.ny_dim
+        hypopt, Cov_mat          = self.hypopt, self.Cov_mat
+        stdX, stdY, meanX, meanY = self.X_std, self.Y_std, self.X_mean, self.Y_mean
+        calc_cov_sample          = self.calc_cov_sample
+        invKsample               = self.invKopt
+        Xsample, Ysample         = self.X_norm, self.Y_norm
+        var_out                  = self.var_out
+
+        xnorm = (x - meanX)/stdX
+        mean  = np.zeros(ny_dim)
+        var   = np.zeros(ny_dim)
+
+        for i in range(ny_dim):
+            invK           = invKsample[i]
+            hyper          = hypopt[:,i]
+            ellopt, sf2opt = np.exp(2*hyper[:nx_dim]), np.exp(2*hyper[nx_dim])
+
+            k       = calc_cov_sample(xnorm,Xsample,ellopt,sf2opt)
+            mean[i] = np.matmul(np.matmul(k.T,invK),Ysample[:,i])
+            var[i]  = max(0, sf2opt - np.matmul(np.matmul(k.T,invK),k)) 
+
+        # --- compute un-normalized mean --- #    
+        mean_sample = mean*stdY + meanY
+        var_sample  = var*stdY**2
+        
+        if var_out:
+            return mean_sample, var_sample
+        else:
+            return mean_sample.flatten()[0]
